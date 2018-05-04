7fe939628e4d52d261e3e1822cf90ea26eed1017
Adrian Wiszniewski <Adrian.Wiszniewski@ricoh-usa.com>
2018-05-04 15:43:18 -0400

    edited register module. works

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
-$(function(){
+$(function () {
     let registerBtn = new Register();
     let loginBtn = new Login();
-    
+
     $(".register").click(() => {
         registerBtn.registerButtonAnimate();
     });
     $(".login").click(() => {
         loginBtn.loginButtonAnimate();
     });
-})
+
+// button submits for register and login
+    $(".regBtn").click(function () {
+        let email = $("#emailAddress").val();
+        let pass = $("#regPassword").val();
+        registerBtn.handleSignUp(email, pass);
+    });
+
+    $(".logBtn").click(function () {
+        let user = $("#plateNum").val();
+        let pass = $("#logPassword").val();
+        
+    });
+
+
+
+});
--- modules/register.js
+++ modules/register.js
@@ -20,4 +20,32 @@ class Register {
             left: "45%"
         });
     }
+    
+    handleSignUp(email, password) {
+        
+        if (email.length < 4) {
+          alert('Please enter an email address.');
+          return;
+        }
+        if (password.length < 4) {
+          alert('Please enter a password loner than 4 characters.');
+          return;
+        }
+        // Sign in with email and pass.
+        // [START createwithemail]
+        firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+          // Handle Errors here.
+          let errorCode = error.code;
+          let errorMessage = error.message;
+          // [START_EXCLUDE]
+          if (errorCode == 'auth/weak-password') {
+            alert('The password is too weak.');
+          } else {
+            alert(errorMessage);
+          }
+          console.log(error);
+          // [END_EXCLUDE]
+        });
+        // [END createwithemail]
+      }
 }
