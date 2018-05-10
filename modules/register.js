37bd76ce74497461c2ef9b8da98ac62e7a4e8adb
Adrian Wiszniewski <Adrian.Wiszniewski@ricoh-usa.com>
2018-05-10 07:59:32 -0400

    fixed register.js bug

--- modules/register.js
+++ modules/register.js
@@ -1,67 +1,64 @@
 class Register {
   constructor() {
-
-
   }
 
-    }
-    registerButtonAnimate() {
-        let viewportWidth = $(window).width();
-        let viewportHeight = $(window).height();
-        
-        $("#logo-jumbotron").animate({ 
-            position: "absolute",
-            bottom: viewportHeight / 8,
-            width: "400",
-            height: "200"
-        });
-        $(".logForm").animate({
-            left: "150%"
-            
-        });
-        $(".regForm").animate({
-            left: "45%"
-        });
-    }
-    
-    handleSignUp(email, password) {
-        
-        if (email.length < 4) {
-          alert('Please enter an email address.');
-          return;
-        }
-        if (password.length < 4) {
-          alert('Please enter a password longer than 4 characters.');
-          return;
-        }
-        // Sign in with email and pass.
-        // [START createwithemail]
-        firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
-          // Handle Errors here.
-          let errorCode = error.code;
-          let errorMessage = error.message;
-          // [START_EXCLUDE]
-          if (errorCode == 'auth/weak-password') {
-            alert('The password is too weak.');
-          } else {
-            alert(errorMessage);
-          }
-          console.log(error);
-          // [END_EXCLUDE]
-        }).then
-        // [END createwithemail]
+  registerButtonAnimate() {
+    let viewportWidth = $(window).width();
+    let viewportHeight = $(window).height();
 
+    $("#logo-jumbotron").animate({
+      position: "absolute",
+      bottom: viewportHeight / 8,
+      width: "400",
+      height: "200"
+    });
+    $(".logForm").animate({
+      left: "150%"
+
+    });
+    $(".regForm").animate({
+      left: "45%"
+    });
+  }
+
+  handleSignUp(email, password) {
+
+    if (email.length < 4) {
+      alert('Please enter an email address.');
+      return;
+    }
+    if (password.length < 4) {
+      alert('Please enter a password longer than 4 characters.');
+      return;
+    }
+    // Sign in with email and pass.
+    // [START createwithemail]
+    firebase.auth().createUserWithEmailAndPassword(email, password).catch(function (error) {
+      // Handle Errors here.
+      let errorCode = error.code;
+      let errorMessage = error.message;
+      // [START_EXCLUDE]
+      if (errorCode == 'auth/weak-password') {
+        alert('The password is too weak.');
+      } else {
+        alert(errorMessage);
       }
-      writeUserData(uid, plate, vin, make, model, year) {
+      console.log(error);
+      // [END_EXCLUDE]
+    }).then
+    // [END createwithemail]
 
-        firebase.database().ref(`users/${uid}`).set({
-            plate: plate,
-            vin: vin,
-            make: make,
-            model: model,
-            year: year,
-            messages: { "0": "Welcome to Tagged!" }
-        });
-    } 
+  }
+  writeUserData(uid, plate, vin, make, model, year) {
+
+    firebase.database().ref(`users/${uid}`).set({
+      plate: plate,
+      vin: vin,
+      make: make,
+      model: model,
+      year: year,
+      messages: { "0": "Welcome to Tagged!" }
+    });
+  }
 
 }
