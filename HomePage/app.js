3f6140e6215ee2182c9ef62dabc2298648240421
Adrian Wiszniewski <Adrian.Wiszniewski@ricoh-usa.com>
2018-05-10 16:34:21 -0400

    added deletion function to messages and hover effects

--- HomePage/app.js
+++ HomePage/app.js
@@ -1,31 +1,20 @@
-$(function(){
+$(function () {
     let signOut = new Login();
     let loginHome = new Login();
     let messaging = new Messaging();
-
 
     $("#userSearch").hide();
     $("#messageContent").hide();
     $("#send-button").hide();
 
-    
+
     $(".logOut").click(function () {
         firebase.auth().signOut();
         window.location = '../LandingPage/index.html';
         console.log("signed out");
-     });
-     firebase.auth().onAuthStateChanged((user) => {
-        if (user == null) {
-           window.location = '../LandingPage/index.html';
-        }
-        else {
-            // TODO: start actual work
-            loginHome.renderHomePage();
-            loginHome.renderInbox();
-        }
-      });
-     
-    $("#send-button").on("click", function(e) {
+    });
+
+    $("#send-button").on("click", function (e) {
         e.preventDefault();
         messaging.sendMessage();
         alert("Message Sent!");
@@ -39,7 +28,28 @@ $(function(){
         $("#userSearch").show();
         $("#messageContent").show();
         $("#send-button").show();
-    });   
+    });
+
+
+    firebase.auth().onAuthStateChanged((user) => {
+        if (user == null) {
+            window.location = '../LandingPage/index.html';
+        }
+        else {
+            // TODO: start actual work
+            loginHome.renderHomePage();
+            loginHome.renderInbox();
+
+            //remove message
+            $(".inbox").on("click", ".currentMessage", function () {
+                let textDate = ($("span", this).text());
+                $(this).remove();
+                loginHome.removeMessage(textDate);
+
+
+            });
+        }
+    });
 
 });
 
--- modules/Login.js
+++ modules/Login.js
@@ -1,13 +1,13 @@
 class Login {
   constructor() {
-   
+
   }
   renderHomePage() {
     let uid = firebase.auth().currentUser.uid;
-    return firebase.database().ref(`/users/${uid}`).once('value').then(function(snapshot) {
+    return firebase.database().ref(`/users/${uid}`).once('value').then(function (snapshot) {
       let userData = snapshot.val();
-      console.log(userData);
       
+
       $(".testRe").append(`<p class="plate">${userData.plate}</p>
                           <p>${userData.make}</p>
                           <p>${userData.model}</p>
@@ -18,69 +18,72 @@ class Login {
   }
   renderInbox() {
     let uid = firebase.auth().currentUser.uid;
-      firebase.database().ref(`/users/${uid}/messages`).on('value', function(snapshot) {
-        let messagesObject = snapshot.val();
-        let resultValues = Object.values(messagesObject);
-        resultValues = resultValues.reverse();
-        let resultKeys = Object.keys(messagesObject);
-        resultKeys = resultKeys.reverse();
-        for (var i = 0; i < resultValues.length; i++) {
-          for (var i=0; i <resultKeys.length; i++) {
-            $(".inbox").append(`<p class="message">${resultKeys[i]} : ${resultValues[i]}</p>`);
-          }
+
+    firebase.database().ref(`/users/${uid}/messages`).once('value', function (snapshot) {
+      let messagesObject = snapshot.val();
+      let resultValues = Object.values(messagesObject).reverse();
+      let resultKeys = Object.keys(messagesObject).reverse();
+      
+      for (var i = 0; i < resultValues.length; i++) {
+        for (var i = 0; i < resultKeys.length; i++) {
+          $(".inbox").append(`<p class="currentMessage"><span>${resultKeys[i]}</span> : ${resultValues[i]}</p>`);
         }
-    }); 
+      }
+    });
+  }
+  removeMessage(message) {  
+    let uid = firebase.auth().currentUser.uid;
+      firebase.database().ref(`users/${uid}/messages/${message}`).remove();  
   }
 
   loginButtonAnimate() {
-    
-      let viewportWidth = $(window).width();
-      let viewportHeight = $(window).height();
-      
-      $("#logo-jumbotron").animate({ 
-          position: "absolute",
-          bottom: viewportHeight / 8,
-          width: "400", 
-          height: "200" 
-      });
-      $(".regForm").animate({
-        left: "-150%"
-      });
-      $(".logForm").animate({
-          left: "45%"
-      });
+
+    let viewportWidth = $(window).width();
+    let viewportHeight = $(window).height();
+
+    $("#logo-jumbotron").animate({
+      position: "absolute",
+      bottom: viewportHeight / 8,
+      width: "400",
+      height: "200"
+    });
+    $(".regForm").animate({
+      left: "-150%"
+    });
+    $(".logForm").animate({
+      left: "45%"
+    });
   }
 
-    signIn(email, password) {
-     
-      if (email.length < 4) {
-        alert('Please enter an email address.');
-        return;
-      }
-      if (password.length < 4) {
-        alert('Please enter a password.');
-        return;
-      }
-      // Sign in with email and pass.
-      // [START authwithemail]
-    
-      firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        // [START_EXCLUDE]
-        if (errorCode === 'auth/wrong-password') {
-          alert('Wrong password.');
-        } else {
-          alert(errorMessage);
-        }
-        console.log(error);
-        // [END_EXCLUDE]
-      });
-      // [END authwithemail]
+  signIn(email, password) {
 
+    if (email.length < 4) {
+      alert('Please enter an email address.');
+      return;
     }
-  
+    if (password.length < 4) {
+      alert('Please enter a password.');
+      return;
+    }
+    // Sign in with email and pass.
+    // [START authwithemail]
+
+    firebase.auth().signInWithEmailAndPassword(email, password).catch(function (error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // [START_EXCLUDE]
+      if (errorCode === 'auth/wrong-password') {
+        alert('Wrong password.');
+      } else {
+        alert(errorMessage);
+      }
+      console.log(error);
+      // [END_EXCLUDE]
+    });
+    // [END authwithemail]
+
+  }
+
 }
-  
- 
+
